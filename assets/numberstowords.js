e7e295a3743384ccc0c7197a9fa273fc584e97de
Henry <henrstep@amazon.com>
2019-10-01 10:58:24 -0400

    update files

---
+++ assets/numberstowords.js
@@ -0,0 +1,33 @@
+(function(){
+    console.log("hey");
+    console.log(new Date().getDay());
+
+    function NumbersToWords(){
+
+        let myArray =[];
+
+        for(let i = 1; i <= 30; i++){
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
